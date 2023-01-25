7fd8a215fa7a5d49748dc05485db2efc2510f6ee
srija-pavishetti <119001557+srija-pavishetti@users.noreply.github.com>
2023-01-26 02:41:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
-// NASA API
+
 const count = 10;
 const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
@@ -12,7 +12,7 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-// Scroll To Top, Remove Loader, Show Content
+
 function showContent(page) {
   window.scrollTo({ top: 0, behavior: 'instant' });
   loader.classList.add('hidden');
@@ -26,31 +26,31 @@ function showContent(page) {
 }
 
 function createDOMNodes(page) {
-  // Load ResultsArray or Favorites
+
   const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
   currentArray.forEach((result) => {
-    // Card Container
+
     const card = document.createElement('div');
     card.classList.add('card');
-    // Link
+
     const link = document.createElement('a');
     link.href = result.hdurl;
     link.title = 'View Full Image';
     link.target = '_blank';
-    // Image
+
     const image = document.createElement('img');
     image.src = result.url;
     image.alt = 'NASA Picture of the Day';
     image.loading = 'lazy';
     image.classList.add('card-img-top');
-    // Card Body
+
     const cardBody = document.createElement('div');
     cardBody.classList.add('card-body');
-    // Card Title
+
     const cardTitle = document.createElement('h5');
     cardTitle.classList.add('card-title');
     cardTitle.textContent = result.title;
-    // Save Text
+
     const saveText = document.createElement('p');
     saveText.classList.add('clickable');
     if (page === 'results') {
@@ -60,16 +60,16 @@ function createDOMNodes(page) {
       saveText.textContent = 'Remove Favorite';
       saveText.setAttribute('onclick', `removeFavorite('${result.url}')`);
     }
-    // Card Text
+
     const cardText = document.createElement('p');
     cardText.textContent = result.explanation;
     // Footer Container
     const footer = document.createElement('small');
     footer.classList.add('text-muted');
-    // Date
+
     const date = document.createElement('strong');
     date.textContent = result.date;
-    // Copyright
+
     const copyrightResult = result.copyright === undefined ? '' : result.copyright;
     const copyright = document.createElement('span');
     copyright.textContent = ` ${copyrightResult}`;
